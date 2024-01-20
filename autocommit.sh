97046ce119dc7bd2701085800116d367d1cd3e63
Sebin CM <sebinsebzz2002@gmail.com>
2024-01-21 03:08:22 +0530

    Auto-commit at 2024-01-21 03:08:22

---
+++ autocommit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REPO_PATH=~/dotfiles
+
+cd $REPO_PATH
+
+if [[ -n $(git status -s) ]]; then
+    git add .
+    git commit -m "Auto-commit at $(date +'%Y-%m-%d %H:%M:%S')"
+    git push origin master
+fi
+
